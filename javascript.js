2087b6f525f5e1ef7ddb2849789d3626443cd9c5
Cameron Brown <moontsukuyomi6@gmail.com>
2025-08-14 01:32:55 +0200

    Add: add hash function to handle hashing keys

--- javascript.js
+++ javascript.js
@@ -3,7 +3,20 @@ class HashMap {
     this.loadFactor = 0.8;
     this.capacity = 16;
   }
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+    }
+
+    return hashCode % this.capacity;
+  }
 }
+
+const map = new HashMap();
+console.log(map.hash("Cameron"));
 
 /* 
   LIMITATIONS SNIPPET:
