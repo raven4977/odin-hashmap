7a6c8f19c1b0d98d0906c82988808f5adad7a604
Cameron Brown <moontsukuyomi6@gmail.com>
2025-08-14 17:48:56 +0200

    Add: remove function to handle removing an item using a key, update hashmap array name and logic to use this.capacity as length

--- javascript.js
+++ javascript.js
@@ -27,8 +27,8 @@ class HashMap {
   constructor() {
     this.loadFactor = 0.8;
     this.size = 0;
-    this.buckets = 16;
-    this.hashmap = [];
+    this.capacity = 16;
+    this.buckets = new Array(this.capacity).fill(null);
   }
 
   hash(key) {
@@ -42,12 +42,12 @@ class HashMap {
 
   set(key, value) {
     const index = this.hash(key);
-    if (!this.hashmap[index]) {
-      this.hashmap[index] = new LinkedList();
-      this.hashmap[index].append(key, value);
+    if (!this.buckets[index]) {
+      this.buckets[index] = new LinkedList();
+      this.buckets[index].append(key, value);
       return;
     }
-    let current = this.hashmap[index].head;
+    let current = this.buckets[index].head;
     while (current) {
       if (key === current.key) {
         current.value = value;
@@ -55,7 +55,7 @@ class HashMap {
       }
       current = current.nextNode;
     }
-    this.hashmap[index].append(key, value);
+    this.buckets[index].append(key, value);
   }
 
   get(key) {
@@ -65,8 +65,8 @@ class HashMap {
     if (index < 0 || index >= buckets.length) {
       throw new Error("Trying to access index out of bounds");
     }
-    if (this.hashmap[index]) {
-      let current = this.hashmap[index].head;
+    if (this.buckets[index]) {
+      let current = this.buckets[index].head;
       while (current) {
         if (current.key === key) {
           return current.value;
@@ -84,8 +84,8 @@ class HashMap {
     if (index < 0 || index >= buckets.length) {
       throw new Error("Trying to access index out of bounds");
     }
-    if (this.hashmap[index]) {
-      let current = this.hashmap[index].head;
+    if (this.buckets[index]) {
+      let current = this.buckets[index].head;
       while (current) {
         if (current.key === key) {
           return true;
@@ -94,6 +94,31 @@ class HashMap {
       }
     }
     return false;
+  }
+
+  remove(key) {
+    const index = this.hash(key);
+    console.log(this.buckets.length);
+
+    const buckets = this.buckets;
+    if (index < 0 || index >= buckets.length) {
+      throw new Error("Trying to access index out of bounds");
+    }
+    const arrayValue = this.buckets[index];
+    if (arrayValue) {
+      let current = arrayValue.head;
+      if (this.buckets[index].head.key === key) {
+        this.buckets[index].head = this.buckets[index].head.nextNode;
+        return;
+      }
+      while (current) {
+        if (current.nextNode && current.nextNode.key === key) {
+          current.nextNode = current.nextNode.nextNode;
+          return;
+        }
+        current = current.nextNode;
+      }
+    }
   }
 }
 
@@ -104,8 +129,10 @@ map.set("Cameron", "Brown");
 map.set("Amerons", "Third");
 // map.set("Cameron", "Jarrod");
 // map.set("Coolio", "Gangsta");
-console.log(map.get("Cameron"));
-console.log(map.has("Brandon"));
+map.get("Cameron");
+map.has("Brandon");
+map.remove("Amerons");
+
 console.log(map);
 
 /* 
